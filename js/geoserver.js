951ffe69fca1cc3fa6343c95b096caae5d81e9d1
illan palao <illanpalao@gmail.com>
2015-04-09 11:38:47 +0000

    openlayers3

--- js/geoserver.js
+++ js/geoserver.js
@@ -47,9 +47,12 @@ try{
             }  
             	
 				GEOSERVER.buildBounds=function buildBounds(arr){
+              	var bounds=arr||GEOSERVER.bounds
         			return new OpenLayers.Bounds(arr[0],arr[1],arr[2],arr[3]);
       		}
             GEOSERVER.show=function (id){
+              
+              /***
               var mapa=this.getById(id);
                 // if this is just a coverage or a group of them, disable a few items,
                 // and default to jpeg format
@@ -82,195 +85,223 @@ try{
                 map.addControl(new OpenLayers.Control.MousePosition({element: $('location')}));
 					 map.addControl(new OpenLayers.Control.LayerSwitcher({ roundedCornerColor : '#0a8161' }));
                 map.zoomToExtent(bounds);
+                **/
+              GEOSERVER.initOpenLayers(id);
             }
+            
 }catch(e){}
 
-            // sets the HTML provided into the nodelist element
-            function setHTML(response){
-                document.getElementById('nodelist').innerHTML = response.responseText;
-            };
-            
-            // shows/hide the control panel
-           
-            // Tiling mode, can be 'tiled' or 'untiled'
-            function setTileMode(tilingMode){
-                if (tilingMode == 'tiled') {
-                    untiled.setVisibility(false);
-                    tiled.setVisibility(true);
-                    map.setBaseLayer(tiled);
-                }
-                else {
-                    untiled.setVisibility(true);
-                    tiled.setVisibility(false);
-                    map.setBaseLayer(untiled);
-                }
+
+ var map;
+
+GEOSERVER.initOpenLayers=function(id){
+  
+      var mapa=this.current=this.getById(id);
+      var pureCoverage = false;
+      // if this is just a coverage or a group of them, disable a few items,
+      // and default to jpeg format
+      var format = 'image/png';
+      var bounds = mapa.bounds||this.bounds;
+      if (pureCoverage) {
+        document.getElementById('filterType').disabled = true;
+        document.getElementById('filter').disabled = true;
+        document.getElementById('antialiasSelector').disabled = true;
+        document.getElementById('updateFilterButton').disabled = true;
+        document.getElementById('resetFilterButton').disabled = true;
+        document.getElementById('jpeg').selected = true;
+        format = "image/jpeg";
+      }
+
+      var mousePositionControl = new ol.control.MousePosition({
+        className: 'custom-mouse-position',
+        target: document.getElementById('location'),
+        coordinateFormat: ol.coordinate.createStringXY(5),
+        undefinedHTML: '&nbsp;'
+      });
+      var untiled = new ol.layer.Image({
+        source: new ol.source.ImageWMS({
+          ratio: 1,
+          url: mapa.url ,
+          params: {'FORMAT': format,
+                   'VERSION': '1.1.1',  
+                LAYERS: mapa.layers,
+                STYLES: '',
+          }
+        })
+      });
+
+      var projection = new ol.proj.Projection({
+          code: mapa.projection,
+          units: 'm',
+          axisOrientation: 'neu'
+      });
+  
+      var capas= [] ;
+  		mapa.layers.forEach(function(layerId){
+          capas.push(new ol.layer.Tile({
+          visible: false,
+          source: new ol.source.TileWMS({
+            url: mapa.url ,
+            params: {'FORMAT': format, 
+                     'VERSION': '1.1.1',
+                     tiled: true,
+                  LAYERS: layerId,
+                  STYLES: '',
             }
-            
-            // Transition effect, can be null or 'resize'
-            function setTransitionMode(transitionEffect){
-                if (transitionEffect === 'resize') {
-                    tiled.transitionEffect = transitionEffect;
-                    untiled.transitionEffect = transitionEffect;
-                }
-                else {
-                    tiled.transitionEffect = null;
-                    untiled.transitionEffect = null;
-                }
-            }
-            
-            // changes the current tile format
-            function setImageFormat(mime){
-                // we may be switching format on setup
-                if(tiled == null)
-                  return;
-                  
-                tiled.mergeNewParams({
-                    format: mime
-                });
-                untiled.mergeNewParams({
-                    format: mime
-                });
-                /*
-                var paletteSelector = document.getElementById('paletteSelector')
-                if (mime == 'image/jpeg') {
-                    paletteSelector.selectedIndex = 0;
-                    setPalette('');
-                    paletteSelector.disabled = true;
-                }
-                else {
-                    paletteSelector.disabled = false;
-                }
-                */
-            }
-            
-            // sets the chosen style
-            function setStyle(style){
-                // we may be switching style on setup
-                if(tiled == null)
-                  return;
-                  
-                tiled.mergeNewParams({
-                    styles: style
-                });
-                untiled.mergeNewParams({
-                    styles: style
-                });
-            }
-            
-            // sets the chosen WMS version
-            function setWMSVersion(wmsVersion){
-                // we may be switching style on setup
-                if(wmsVersion == null)
-                  return;
-                  
-                if(wmsVersion == "1.3.0") {
-                   origin = map.maxExtent.bottom + ',' + map.maxExtent.left;
-                } else {
-                   origin = map.maxExtent.left + ',' + map.maxExtent.bottom;
-                }
-                  
-                tiled.mergeNewParams({
-                    version: wmsVersion,
-                    tilesOrigin : origin
-                });
-                untiled.mergeNewParams({
-                    version: wmsVersion
-                });
-            }
-            
-            function setAntialiasMode(mode){
-                tiled.mergeNewParams({
-                    format_options: 'antialias:' + mode
-                });
-                untiled.mergeNewParams({
-                    format_options: 'antialias:' + mode
-                });
-            }
-            
-            function setPalette(mode){
-                if (mode == '') {
-                    tiled.mergeNewParams({
-                        palette: null
-                    });
-                    untiled.mergeNewParams({
-                        palette: null
-                    });
-                }
-                else {
-                    tiled.mergeNewParams({
-                        palette: mode
-                    });
-                    untiled.mergeNewParams({
-                        palette: mode
-                    });
-                }
-            }
-            
-            function setWidth(size){
-                var mapDiv = document.getElementById('map');
-                var wrapper = document.getElementById('wrapper');
-                
-                if (size == "auto") {
-                    // reset back to the default value
-                    mapDiv.style.width = null;
-                    wrapper.style.width = null;
-                }
-                else {
-                    mapDiv.style.width = size + "px";
-                    wrapper.style.width = size + "px";
-                }
-                // notify OL that we changed the size of the map div
-                map.updateSize();
-            }
-            
-            function setHeight(size){
-                var mapDiv = document.getElementById('map');
-                
-                if (size == "auto") {
-                    // reset back to the default value
-                    mapDiv.style.height = null;
-                }
-                else {
-                    mapDiv.style.height = size + "px";
-                }
-                // notify OL that we changed the size of the map div
-                map.updateSize();
-            }
-            
-            function updateFilter(){
-                if(pureCoverage)
-                  return;
-            
-                var filterType = document.getElementById('filterType').value;
-                var filter = document.getElementById('filter').value;
-                
-                // by default, reset all filters
-                var filterParams = {
-                    filter: null,
-                    cql_filter: null,
-                    featureId: null
-                };
-                if (OpenLayers.String.trim(filter) != "") {
-                    if (filterType == "cql") 
-                        filterParams["cql_filter"] = filter;
-                    if (filterType == "ogc") 
-                        filterParams["filter"] = filter;
-                    if (filterType == "fid") 
-                        filterParams["featureId"] = filter;
-                }
-                // merge the new filter definitions
-                mergeNewParams(filterParams);
-            }
-            
-            function resetFilter() {
-                if(pureCoverage)
-                  return;
-            
-                document.getElementById('filter').value = "";
-                updateFilter();
-            }
-            
-            function mergeNewParams(params){
-                tiled.mergeNewParams(params);
-                untiled.mergeNewParams(params);
-            }
+          })
+        }));
+      });  
+          map = this.map = new ol.Map({
+            controls: ol.control.defaults({
+              attribution: false
+            }).extend([mousePositionControl]),
+            target: 'map',
+            layers: capas,
+            view: new ol.View({
+               projection: projection
+            })
+          });
+      map.getView().on('change:resolution', function(evt) {
+        var resolution = evt.target.get('resolution');
+        var units = map.getView().getProjection().getUnits();
+        var dpi = 25.4 / 0.28;
+        var mpu = ol.proj.METERS_PER_UNIT[units];
+        var scale = resolution * mpu * 39.37 * dpi;
+        if (scale >= 9500 && scale <= 950000) {
+          scale = Math.round(scale / 1000) + "K";
+        } else if (scale >= 950000) {
+          scale = Math.round(scale / 1000000) + "M";
+        } else {
+          scale = Math.round(scale);
+        }
+        document.getElementById('scale').innerHTML = "Scale = 1 : " + scale;
+      });
+      map.getView().fitExtent(bounds, map.getSize());
+      map.on('singleclick', function(evt) {
+        document.getElementById('nodelist').innerHTML = "Loading... please wait...";
+        var view = map.getView();
+        var viewResolution = view.getResolution();
+        var source = untiled.get('visible') ? untiled.getSource() : tiled.getSource();
+        var url = source.getGetFeatureInfoUrl(
+          evt.coordinate, viewResolution, view.getProjection(),
+          {'INFO_FORMAT': 'text/html', 'FEATURE_COUNT': 50});
+        if (url) {
+          document.getElementById('nodelist').innerHTML = '<iframe seamless src="' + url + '"></iframe>';
+        }
+      });
+
+      // sets the chosen WMS version
+      function setWMSVersion(wmsVersion) {
+        map.getLayers().forEach(function(lyr) {
+          lyr.getSource().updateParams({'VERSION': wmsVersion});
+        });
+      }
+
+      // Tiling mode, can be 'tiled' or 'untiled'
+      function setTileMode(tilingMode) {
+        if (tilingMode == 'tiled') {
+          untiled.set('visible', false);
+          tiled.set('visible', true);
+        } else {
+          tiled.set('visible', false);
+          untiled.set('visible', true);
+        }
+      }
+
+      function setAntialiasMode(mode) {
+        map.getLayers().forEach(function(lyr) {
+          lyr.getSource().updateParams({'FORMAT_OPTIONS': 'antialias:' + mode});
+        });
+      }
+
+      // changes the current tile format
+      function setImageFormat(mime) {
+        map.getLayers().forEach(function(lyr) {
+          lyr.getSource().updateParams({'FORMAT': mime});
+        });
+      }
+
+      function setStyle(style){
+        map.getLayers().forEach(function(lyr) {
+          lyr.getSource().updateParams({'STYLES': style});
+        });
+      }
+
+      function setWidth(size){
+        var mapDiv = document.getElementById('map');
+        var wrapper = document.getElementById('wrapper');
+
+        if (size == "auto") {
+          // reset back to the default value
+          mapDiv.style.width = null;
+          wrapper.style.width = null;
+        }
+        else {
+          mapDiv.style.width = size + "px";
+          wrapper.style.width = size + "px";
+        }
+        // notify OL that we changed the size of the map div
+        map.updateSize();
+      }
+
+      function setHeight(size){
+        var mapDiv = document.getElementById('map');
+        if (size == "auto") {
+          // reset back to the default value
+          mapDiv.style.height = null;
+        }
+        else {
+          mapDiv.style.height = size + "px";
+        }
+        // notify OL that we changed the size of the map div
+        map.updateSize();
+      }
+
+      function updateFilter(){
+        if (pureCoverage) {
+          return;
+        }
+        var filterType = document.getElementById('filterType').value;
+        var filter = document.getElementById('filter').value;
+        // by default, reset all filters
+        var filterParams = {
+          'FILTER': null,
+          'CQL_FILTER': null,
+          'FEATUREID': null
+        };
+        if (filter.replace(/^\s\s*/, '').replace(/\s\s*$/, '') != "") {
+          if (filterType == "cql") {
+            filterParams["CQL_FILTER"] = filter;
+          }
+          if (filterType == "ogc") {
+            filterParams["FILTER"] = filter;
+          }
+          if (filterType == "fid")
+            filterParams["FEATUREID"] = filter;
+          }
+          // merge the new filter definitions
+          map.getLayers().forEach(function(lyr) {
+            lyr.getSource().updateParams(filterParams);
+          });
+        }
+
+        function resetFilter() {
+          if (pureCoverage) {
+            return;
+          }
+          document.getElementById('filter').value = "";
+          updateFilter();
+        }
+
+        // shows/hide the control panel
+        function toggleControlPanel(){
+          var toolbar = document.getElementById("toolbar");
+          if (toolbar.style.display == "none") {
+            toolbar.style.display = "block";
+          }
+          else {
+            toolbar.style.display = "none";
+          }
+          map.updateSize()
+        }
+}
